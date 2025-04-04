602b687d93e7a015fee251d734a3f3e3681ae417
ThaiBaBang130706 <bangkbak@gmail.com>
2025-04-04 15:55:15 +0700

    Add files via upload

---
+++ tuan 7/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Student student = new Student("babang", "249 TDN", "laptop", 1, 0);
+        Staff staff = new Staff("VKU", 998, "VH", "NHS");
+
+        System.out.println(student);
+        System.out.println(staff);
+    }
+}
